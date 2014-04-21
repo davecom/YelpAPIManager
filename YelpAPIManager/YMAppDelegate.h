56f800580d369cde055761523542ea4caa9636ac
lonelytango <doomon@gmail.com>
2014-04-20 23:28:59 -0400

    Adding project.

---
+++ YelpAPIManager/YMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YMAppDelegate.h
+//  YelpAPIManager
+//
+//  Created by Zian Chen on 4/20/14.
+//  Copyright (c) 2014 Zian Mobile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YelpAPIManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YelpAPIManager
+//
+//  Created by Zian Chen on 4/20/14.
+//  Copyright (c) 2014 Zian Mobile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YMAppDelegate class]));
+    }
+}
